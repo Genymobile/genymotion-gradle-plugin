6bcf83ecb2215e81ce471f885037c281e2627caa
Eyal LEZMY <elezmy@genymobile.com>
2014-09-11 17:25:17 +0200

    [US241.3] Adding commands inside GenymotionTask

--- plugin/src/main/groovy/com/genymotion/GenymotionTask.groovy
+++ plugin/src/main/groovy/com/genymotion/GenymotionTask.groovy
@@ -1,6 +1,6 @@
 package com.genymotion
 
-import main.groovy.com.genymotion.GenymotionVirtualDevice
+import main.groovy.com.genymotion.GenymotionTool
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -9,15 +9,27 @@ class GenymotionTask extends DefaultTask {
     @TaskAction
     def exec() {
 
+        //we set the config inside the GenymotionTool
+        GenymotionTool.CONFIG = project.genymotion.config
+
         println("Starting devices")
         //get the declared devices
         project.genymotion.getDevices().each(){
-            println("Starting ${it.name}")
-            def cmd = [project.genymotion.config.genymotionPath+"player", '--vm-name', it.name]
 
-            def process = cmd.execute()
-            println process.text
+            println("Starting ${it.name}")
+            GenymotionTool.startDevice(it)
         }
+
+        sleep 5000
+
+        println("-- All devices --")
+        def result = GenymotionTool.getAllDevices(true)
+        println("-- Running devices --")
+        GenymotionTool.getRunningDevices(true)
+        println("-- Stopped devices --")
+        GenymotionTool.stoppedDevices
+
+
 
 
 //        println "${project.genymotionArgs.message} from ${project.genymotionArgs.sender}"
