7805b04e9f233b07fe9c3ceaff7c94a94a542512
Eyal LEZMY <elezmy@genymobile.com>
2014-09-17 13:46:36 +0200

    [US241.3] Injecting genymotion task just before connectedAndroidTest

--- plugin/src/main/groovy/com/genymotion/GenymotionGradlePlugin.groovy
+++ plugin/src/main/groovy/com/genymotion/GenymotionGradlePlugin.groovy
@@ -10,37 +10,27 @@ import org.gradle.api.Plugin
 class GenymotionGradlePlugin implements Plugin<Project> {
 
     void apply(Project project) {
+
+        println "adding Genymotion plugin"
         project.extensions.create('genymotion', GenymotionPluginExtension, project);
         project.genymotion.extensions.create('config', GenymotionConfig); //the extension name have to be different from the original nested element's name (receiver)
         project.genymotion.extensions.create('admin', GenymotionAdmin); //the extension name have to be different from the original nested element's name (receiver)
         project.genymotion.extensions.create('devices', GenymotionDevices); //the extension name have to be different from the original nested element's name (receiver)
 
+        project.task('genymotion', type: GenymotionTask)
+        project.task('cmd', type: CmdTask)
 
         if(project.plugins.hasPlugin('android')){
 
-            //inject an action before compiling to launch devices
-            preBuild.doFirst {
+            println "Android plugin detected"
 
-                println "Do before preBuilt"
-                //execute the genymotion.admin commands
-                //TODO
-
-                //start and configure genymotion.devices
-                //TODO
-                println "Done before preBuilt"
-
-            }
-
-            connectedAndroidTest.doLast{
-                //inject an event after testing to close devices
-                //TODO
-
-                println "Done after connectedAndroidTest"
+            project.tasks.whenTaskAdded { theTask ->
+                if("connectedAndroidTest".toString().equals(theTask.name.toString())){
+                    println "Adding genymotion dependency"
+                    theTask.dependsOn("genymotion")
+                }
             }
         }
 
-
-        project.task('genymotion', type: GenymotionTask)
-        project.task('cmd', type: CmdTask)
     }
 }
