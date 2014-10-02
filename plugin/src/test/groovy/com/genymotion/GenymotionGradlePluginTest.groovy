562c2122744869b672775bffbf788c45fe074c09
Eyal LEZMY <elezmy@genymobile.com>
2014-10-03 01:13:17 +0200

    [US241.3] Improving GenymotionGradlePluginTest

--- plugin/src/test/groovy/com/genymotion/GenymotionGradlePluginTest.groovy
+++ plugin/src/test/groovy/com/genymotion/GenymotionGradlePluginTest.groovy
@@ -1,16 +1,36 @@
 package com.genymotion
 
+import main.groovy.com.genymotion.GenymotionAdmin
+import main.groovy.com.genymotion.GenymotionConfig
+import main.groovy.com.genymotion.GenymotionDevices
+import main.groovy.com.genymotion.GenymotionVirtualDevice
+import org.junit.Before
 import org.junit.Test
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.api.Project
 import static org.junit.Assert.*
 
 class GenymotionGradlePluginTest {
-    @Test
-    public void genymotionPluginAddsGreetingTaskToProject() {
-        Project project = ProjectBuilder.builder().build()
-        project.apply plugin: 'genymotion'
 
-        assertTrue(project.tasks.hello instanceof GreetingTask)
+    Project project
+
+    @Before
+    public void setUp() {
+        project = ProjectBuilder.builder().build()
+        project.apply plugin: 'genymotion'
     }
+
+    @Test
+    public void canAddsTaskToProject() {
+        assertTrue(project.tasks.genymotionLaunch instanceof GenymotionTask)
+    }
+
+    @Test
+    public void canAddExtensionToProject() {
+        assertTrue(project.genymotion instanceof GenymotionPluginExtension)
+        assertTrue(project.genymotion.config instanceof GenymotionConfig)
+        assertTrue(project.genymotion.admin instanceof GenymotionAdmin)
+        assertTrue(project.genymotion.devices instanceof GenymotionVirtualDevice[])
+    }
+
 }
