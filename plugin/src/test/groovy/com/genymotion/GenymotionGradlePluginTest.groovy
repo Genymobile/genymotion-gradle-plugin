510e83755273a3ecfda4ff212344f7af0c248e86
Eyal LEZMY <elezmy@genymobile.com>
2014-10-08 18:26:20 +0200

    [US241.3] Extending tests on GenymotionGradlePluginTest

--- plugin/src/test/groovy/com/genymotion/GenymotionGradlePluginTest.groovy
+++ plugin/src/test/groovy/com/genymotion/GenymotionGradlePluginTest.groovy
@@ -46,4 +46,197 @@ class GenymotionGradlePluginTest {
         assertTrue(project.genymotion.devices instanceof List)
     }
 
+    @Test
+    public void canConfigGenymotion(){
+        String path = "TEST"
+        project.genymotion.config.genymotionPath = path
+
+        assertEquals(path, project.genymotion.config.genymotionPath)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenAddDeviceWithoutParams(){
+
+        project.genymotion.device(null)
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenAddDeviceWithoutNameAndTemplate(){
+
+        project.genymotion.device(hola:"buenos dias")
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenAddDeviceWithNameNotCreated(){
+
+        project.genymotion.device(name:"DSFGTFSHgfgdfTFGQFQHG")
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenAddDeviceWithTemplateNotCreated(){
+
+        project.genymotion.device(template: "DSFGTFSHgfgdfTFGQFQHG")
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsWhenAddDeviceWithNameAndTemplateNotCreated(){
+
+        project.genymotion.device(name:"DSFGTFSHTFGQFQHG", template:"ferrfgfgdshghGFGDFGfgfd")
+    }
+
+    @Test
+    public void throwsWhenAddDeviceWithNameAndTemplateNotCreated2(){
+
+        project.genymotion.device(name:"DSFGTFSHTFGQFQHG", template:"Google Nexus 7 - 4.1.1 - API 16 - 800x1280")
+    }
+
+    @Test
+    public void canAddDeviceToLaunchByName(){
+
+        String vdName = GenymotionToolTest.createADevice()
+
+        project.genymotion.device(name:vdName)
+        assertNull(project.genymotion.devices[0].template)
+        assertEquals(vdName, project.genymotion.devices[0].name)
+
+        GenymotionTool.deleteDevice(vdName)
+    }
+
+    @Test
+    public void canAddDeviceToLaunchByNameWithTemplate(){
+
+        String vdName = GenymotionToolTest.createADevice()
+
+        project.genymotion.device(name:vdName, template: "Google Nexus 7 - 4.1.1 - API 16 - 800x1280")
+        assertNull(project.genymotion.devices[0].template)
+        assertEquals(vdName, project.genymotion.devices[0].name)
+
+        GenymotionTool.deleteDevice(vdName)
+    }
+
+    @Test
+    public void canAddDeviceToLaunchByNameWithTemplateNotCreated(){
+
+        String vdName = GenymotionToolTest.createADevice()
+
+        project.genymotion.device(name:vdName, template: "frtfgfdgtgsgrGFGFDGFD")
+        assertNull(project.genymotion.devices[0].template)
+        assertEquals(vdName, project.genymotion.devices[0].name)
+
+        GenymotionTool.deleteDevice(vdName)
+    }
+
+    @Test
+    public void canAddDeviceToLaunchByTemplate(){
+
+        project.genymotion.device(template:"Google Nexus 7 - 4.1.1 - API 16 - 800x1280")
+
+        assertNotNull(project.genymotion.devices[0])
+        assertNotNull(project.genymotion.devices[0].name)
+        assertTrue(project.genymotion.devices[0].create)
+        assertTrue(project.genymotion.devices[0].deleteWhenFinish)
+    }
+
+    @Test
+    public void canAddDeviceToLaunchByTemplateWithNameNotCreated(){
+
+        project.genymotion.device(template:"Google Nexus 7 - 4.1.1 - API 16 - 800x1280", name: "dfsdgffgdgqsdg")
+
+        assertNotNull(project.genymotion.devices[0])
+        assertNotNull(project.genymotion.devices[0].name)
+        assertTrue(project.genymotion.devices[0].create)
+        assertFalse(project.genymotion.devices[0].deleteWhenFinish)
+    }
+
+
+    @Test
+    public void canAvoidDeviceToBeLaunched(){
+
+        project.genymotion.device(template:"Google Nexus 7 - 4.1.1 - API 16 - 800x1280", start: false)
+
+        assertFalse(project.genymotion.devices[0].start)
+    }
+
+    @Test
+    public void canUpdateDeviceBeforeLaunch(){
+
+        String vdName = "OKOK"
+        GenymotionTool.deleteDevice(vdName)
+
+        int intValue = 999
+
+        project.genymotion.device(
+                name: vdName,
+                template:"Google Nexus 7 - 4.1.1 - API 16 - 800x1280",
+                dpi: intValue,
+                width: intValue,
+                height: intValue,
+                physicalButton: false,
+                navbar: false,
+                nbCpu: 1,
+                ram: 2048
+        )
+
+        assertNotNull(project.genymotion.devices[0])
+        assertEquals(project.genymotion.devices[0].name, vdName)
+
+        project.genymotion.devices[0].create()
+        project.genymotion.devices[0].checkAndUpdate()
+
+        GenymotionVirtualDevice device = GenymotionTool.getDevice(vdName, true)
+        assertEquals(intValue, device.dpi)
+        assertEquals(intValue, device.width)
+        assertEquals(intValue, device.height)
+//        assertEquals(false, device.physicalButton) //TODO uncomment when bug on gmtool is fixed
+//        assertEquals(false, device.navbar) //TODO uncomment when bug on gmtool is fixed
+        assertEquals(1, device.nbCpu)
+        assertEquals(2048, device.ram)
+
+        GenymotionTool.deleteDevice(vdName)
+    }
+
+/*
+    @Test
+    public void canSetDeleteWhenFinish(){
+
+    }
+*/
+
+/*
+    @Test
+    public void canPushToDevice(){
+
+    }
+*/
+
+
+/*
+    @Test
+    public void canPushToDevice(){
+
+    }
+*/
+
+/*
+    @Test
+    public void canPullFromDevice(){
+
+    }
+*/
+
+/*
+    @Test
+    public void canInstallAPK(){
+
+    }
+*/
+
+/*
+    @Test
+    public void canFlashDevice(){
+
+    }
+*/
+
 }
