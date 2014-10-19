953c8524469571915f3d5685df6358632ca40b2a
Eyal LEZMY <elezmy@genymobile.com>
2014-10-19 18:19:59 +0200

    [US241.3] Handling gmtool error during EndTask

--- plugin/src/main/groovy/com/genymotion/GenymotionEndTask.groovy
+++ plugin/src/main/groovy/com/genymotion/GenymotionEndTask.groovy
@@ -16,11 +16,31 @@ class GenymotionEndTask extends DefaultTask {
             println("Stopping ${it.name}")
             //TODO check if the device is already started
             if(it.start) {
-                it.pushAfter()
-                it.pullAfter()
-                GenymotionTool.stopDevice(it)
-                if(it.deleteWhenFinish)
-                    GenymotionTool.deleteDevice(it)
+
+                try{
+                    it.pushAfter()
+                    it.pullAfter()
+                    GMTool.stopDevice(it)
+                    if(it.deleteWhenFinish)
+                        GMTool.deleteDevice(it)
+                }
+                //if a gmtool command fail
+                catch(Exception e){
+
+                    println e.getMessage()
+                    println "Stoping all launched devices and deleting when needed"
+                    project.genymotion.getDevices().each(){
+                        //we close the opened devices
+                        GMTool.stopDevice(it)
+                        //and delete them if needed
+                        if(it.deleteWhenFinish)
+                            GMTool.deleteDevice(it)
+                    }
+                    //then, we thow a new exception to end task, if needed
+                    if(project.genymotion.config.abortOnError)
+                        throw new GMToolException("GMTool command failed. Check the output to solve the problem")
+                }
+
             }
         }
     }
