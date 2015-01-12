d3cfd49f61dd13aabf21cb976e5f79e3554772e3
Eyal LEZMY <elezmy@genymobile.com>
2015-01-12 11:44:15 +0100

    [US241] Fix bytecode error with @CompileStatic

--- plugin/src/main/groovy/com/genymotion/GMToolException.groovy
+++ plugin/src/main/groovy/com/genymotion/GMToolException.groovy
@@ -1,10 +1,13 @@
 package main.groovy.com.genymotion
 
+import groovy.transform.CompileStatic
+
 import java.lang.Exception
 
 /**
  * Created by eyal on 17/10/14.
  */
+@CompileStatic
 class GMToolException extends Exception {
     GMToolException(){
         super()
