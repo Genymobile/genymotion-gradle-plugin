78cf839cc9359f2c9214935199ee8745617d78cb
Eyal LEZMY <elezmy@genymobile.com>
2015-10-19 09:55:16 +0200

    Fix for a better self explaining code

--- plugin/src/main/groovy/com/genymotion/tools/Log.groovy
+++ plugin/src/main/groovy/com/genymotion/tools/Log.groovy
@@ -29,7 +29,7 @@ class Log {
         if (logger == null) {
             logger = LoggerFactory.getLogger("genymotion-logger")
         }
-        logger
+        return logger
     }
 
     static synchronized clearLogger() {
