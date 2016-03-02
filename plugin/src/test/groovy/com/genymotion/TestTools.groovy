6f41d808d61caa323a6d759abc9f4052818db33f
Eyal Lezmy <eyal@lezmy.fr>
2016-03-02 02:03:31 +0100

    Clean TestTool and use mocks by default when init projects

--- plugin/src/test/groovy/com/genymotion/TestTools.groovy
+++ plugin/src/test/groovy/com/genymotion/TestTools.groovy
@@ -24,179 +24,25 @@ import com.genymotion.tools.GMTool
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 
+import static org.mockito.Mockito.*
+
 class TestTools {
 
-    public static
-    final String[] RANDOM_NAMES = ["Sam", "Julien", "Dan", "Pascal", "Guillaume", "Damien", "Thomas", "Sylvain", "Philippe", "Cedric", "Charly", "Morgan", "Bruno"]
+    public static final String[] RANDOM_NAMES = ["Sam", "Julien", "Dan", "Pascal", "Guillaume", "Damien", "Thomas", "Sylvain", "Philippe", "Cedric", "Charly", "Morgan", "Bruno"]
 
-    public static String TEMP_PATH = "temp" + File.separator
-    public static String PULLED_PATH = TEMP_PATH + "pulled" + File.separator
-
-    public static def DEVICES = [
-            "Nexus7-junit" : "Google Nexus 7 - 4.1.1 - API 16 - 800x1280",
-            "Nexus10-junit": "Google Nexus 10 - 4.4.4 - API 19 - 2560x1600",
-            "Nexus4-junit" : "Google Nexus 4 - 4.3 - API 18 - 768x1280"
-    ]
-
-    static def init() {
+    static def init(def gmtool = mock(GMTool)) {
 
         Project project = ProjectBuilder.builder().build()
         project.apply plugin: 'genymotion'
-        setDefaultGenymotionPath(project)
 
         GMTool.DEFAULT_CONFIG = project.genymotion.config
-        GMTool gmtool = GMTool.newInstance()
-        gmtool.getConfig(project.genymotion.config, true)
+
         project.genymotion.config.verbose = true
 
         [project, gmtool]
     }
 
-    private static void setDefaultGenymotionPath(Project project, String defaultPath = null) {
-        String path = getDefaultConfig()?.genymotionPath
-        if (path) {
-            project.genymotion.config.genymotionPath = path
-        } else if (defaultPath) {
-            project.genymotion.config.genymotionPath = defaultPath
-        } else {
-            project.genymotion.config.genymotionPath = GenymotionConfig.getDefaultGenymotionPath()
-        }
-    }
-
-    static void deleteAllDevices(GMTool gmtool) {
-        DEVICES.each() { key, value ->
-            gmtool.deleteDevice(key)
-        }
-    }
-
-    static void createAllDevices(GMTool gmtool) {
-        DEVICES.each() { key, value ->
-            gmtool.createDevice(value, key)
-        }
-    }
-
-    static String createADevice(GMTool gmtool) {
-
-        Random rand = new Random()
-        int index = rand.nextInt(DEVICES.size())
-
-        String[] keys = DEVICES.keySet() as String[]
-        String name = keys[index]
-        gmtool.createDevice(DEVICES[name], name)
-
-        name
-    }
-
-    static def declareADetailedDevice(Project project, boolean stop = true) {
-        String vdName = getRandomName("-junit")
-        String densityName = "mdpi"
-        int heightInt = 480
-        int widthInt = 320
-        int ramInt = 2048
-        int nbCpuInt = 1
-        boolean delete = true
-
-        project.genymotion.devices {
-            "$vdName" {
-                template "Google Nexus 7 - 4.1.1 - API 16 - 800x1280"
-                density densityName
-                width widthInt
-                height heightInt
-                virtualKeyboard false
-                navbarVisible false
-                nbCpu nbCpuInt
-                ram ramInt
-                deleteWhenFinish delete
-                stopWhenFinish stop
-            }
-        }
-        [vdName, densityName, widthInt, heightInt, nbCpuInt, ramInt, delete]
-    }
-
-
-    static void cleanAfterTests(GMTool gmtool) {
-
-        println "Cleaning after tests"
-
-        gmtool.getConfig(true)
-
-        try {
-            def devices = gmtool.getAllDevices(false, false, false)
-            def pattern = ~/^.+?\-junit$/
-            println devices
-
-            devices.each() {
-                if (pattern.matcher(it.name).matches()) {
-                    println "Removing $it.name"
-                    if (it.isRunning()) {
-                        gmtool.stopDevice(it.name, true)
-                    }
-                    gmtool.deleteDevice(it.name, true)
-                }
-            }
-        } catch (Exception e) {
-            println e
-        }
-
-        new File("temp").deleteDir()
-    }
-
-    static void recreatePulledDirectory() {
-        File tempDir = new File(PULLED_PATH)
-        if (tempDir.exists()) {
-            if (tempDir.isDirectory()) {
-                tempDir.deleteDir()
-            } else {
-                tempDir.delete()
-            }
-        }
-        tempDir.mkdirs()
-    }
-
-    static GenymotionConfig getDefaultConfig(String path = "res/test/default.properties") {
-        GenymotionConfig config = new GenymotionConfig()
-        config.fromFile = path
-
-        if (config.applyConfigFromFile(null)) {
-            return config
-        }
-
-        return null
-    }
-
-    static setDefaultUser(registerLicense = false, GMTool gmtool) {
-        gmtool.resetConfig()
-        GenymotionConfig config = getDefaultConfig()
-        config.version = gmtool.getVersion()
-        gmtool.genymotionConfig.version = config.version
-
-        if (!config) {
-            return
-        }
-
-        if (config.username && config.password) {
-            gmtool.setConfig(config, true)
-
-            if (config.license && registerLicense) {
-                gmtool.setLicense(config.license, true)
-            }
-        }
-    }
-
-    static String getRandomName(String extension = null) {
-        int nameLength = 3
-        String name = ""
-        Random r = new Random()
-        nameLength.times() {
-            name += RANDOM_NAMES[r.nextInt(RANDOM_NAMES.size())]
-        }
-        if (extension) {
-            name += extension
-        }
-        name
-    }
-
-    static Project getAndroidProject() {
+    static def getAndroidProject(def gmtool = null) {
 
         Project project = ProjectBuilder.builder().withProjectDir(new File("res/test/android-app")).build();
 
@@ -207,12 +53,15 @@ class TestTools {
             compileSdkVersion 23
             buildToolsVersion "23.0.2"
         }
-        project.genymotion.config.genymotionPath = TestTools.getDefaultConfig().genymotionPath
 
         project.afterEvaluate {
             println "TASKS AFTER " + project.tasks
         }
 
-        return project
+        if (gmtool == null) {
+            gmtool = mock(GMTool)
+        }
+
+        [project, gmtool]
     }
 }
