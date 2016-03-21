e13951878f21898cd7deadbc4cfe8e424991217d
flo <fnoel@genymobile.com>
2016-03-21 12:18:57 +0100

    Make test script work on windows machine

--- ci/test_android_compat.py
+++ ci/test_android_compat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from os import path
+import platform
 from subprocess import check_call, call
 
 PROJECT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
@@ -11,19 +12,23 @@ ANDROID_TO_GRADLE = {
     "2.10": ["+", "1.5.0"],
 }
 
+if platform.system() == "Windows":
+    GRADLEW = "gradlew.bat"
+else:
+    GRADLEW = 'gradlew'
 
 def main():
     gradle_versions = sorted(ANDROID_TO_GRADLE.keys()) # we want to finish with 2.2
     try:
         for gradle_version in gradle_versions:
             # Set the current project to the good Gradle version
-            cmd = [PROJECT_DIR + path.sep + 'gradlew', 'wrapper', '--gradle-version', gradle_version]
+            cmd = [PROJECT_DIR + path.sep + GRADLEW, 'wrapper', '--gradle-version', gradle_version]
             print(cmd)
             check_call(cmd)
 
             for android_version in ANDROID_TO_GRADLE[gradle_version]:
                 # Launch the Genymotion Gradle plugin tests using the current Android Gradle plugin version
-                cmd = [PROJECT_DIR + path.sep + 'gradlew', ':plugin:test', '--stacktrace', '-PincludeCat=com.genymotion.Android',
+                cmd = [PROJECT_DIR + path.sep + GRADLEW, ':plugin:test', '--stacktrace', '-PincludeCat=com.genymotion.Android',
                        '-PandroidPluginVersion=' + android_version]
                 print(cmd)
                 check_call(cmd)
